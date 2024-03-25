c0dd3acf20b7712832e6bec1b69025a85423d8fe
Kiloechoalpha <letlhake.kv@gmail.com>
2024-03-25 15:35:43 +0200

    Add Task 2 solution

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function removeDuplicates() {
     fs.writeFileSync('uniqueUsers.json', JSON.stringify(uniqueUsers.map(user => JSON.parse(user)), null, 2));
 }
 
+// Task 2: Create CSV file with name, surname, and number of times duplicated
+function createCSV() {
+    const usersData = require('./usersBackEnd.json');
+    const userCountMap = new Map();
+    usersData.forEach(user => {
+        const key = user.name + ' ' + user.surname;
+        userCountMap.set(key, (userCountMap.get(key) || 0) + 1);
+    });
+
+    let csvContent = "Name,Surname,Number of times duplicated\n";
+    userCountMap.forEach((count, user) => {
+        const [name, surname] = user.split(' ');
+        csvContent += `"${name}","${surname}",${count}\n`;
+    });
+
+    fs.writeFileSync('usersCount.csv', csvContent);
+}
